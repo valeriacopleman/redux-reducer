145f923a353388ffe144e4cd1b9bba2ac3adcef0
Valeria Copleman <valeriacopleman@gmail.com>
2021-04-04 21:14:21 -0700

    commit tree 3bd66af5cde54b68c957e8f0a56665029eb2475e

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,20 @@
 export function manageFriends(state, action){
+    switch (action.type) {
+        case 'ADD_FRIEND':
+          return {
+            ...state,
+            friends: [...state.friends, {
+                hometown: action.friend.hometown,
+                id:action.friend.id,
+                name:action.friend.name,
+            }]
+          }
+        case 'REMOVE_FRIEND':
+            let newFriends = state.friends.filter(friend => 
+                {return friend.id !== action.id})
+            return {friends: newFriends};
+       
+        default:
+          return state;
+      }
 }

e89f543e633eec57fe4476262c919fe35105d465
Valeria Copleman <valeriacopleman@gmail.com>
2021-04-04 21:16:21 -0700

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,20 @@
 export function manageFriends(state, action){
+    switch (action.type) {
+        case 'ADD_FRIEND':
+          return {
+            ...state,
+            friends: [...state.friends, {
+                hometown: action.friend.hometown,
+                id:action.friend.id,
+                name:action.friend.name,
+            }]
+          }
+        case 'REMOVE_FRIEND':
+            let newFriend = state.friends.filter(friend => 
+                {return friend.id !== action.id})
+            return {friends: newFriend};
+       
+        default:
+          return state;
+      }
 }
